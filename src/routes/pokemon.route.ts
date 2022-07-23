5b7bdbadc14589535745c836118b693758e5ea7a
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-23 12:46:21 -0300

    project cleanup

--- src/routes/pokemon.route.ts
+++ src/routes/pokemon.route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import pokedexController from '../controllers/pokedex.controller';
+
 const router = express.Router();
 
 router.get('/pokemon/:limit?/:offset?', pokedexController.listPokemon);
--- src/services/pokedex.service.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export class PokedexService {
-  public testMessage(req: Request, res: Response) {
-    return res.status(200).send("test");
-  }
-}
